519c5e1511ca391f4f516ed9ee4e6721487f7c6c
gabor00 <garaigabor2@gmail.com>
2023-05-23 16:50:46 +0200

    a jelszó randomizálása a user készítésénél

--- app/Console/Commands/InitApp.php
+++ app/Console/Commands/InitApp.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
 use Faker\Factory as Faker;
+use Illuminate\Support\Str;
 
 class InitApp extends Command
 {
@@ -17,12 +18,12 @@ class InitApp extends Command
         $faker = Faker::create();
         $name = $faker->name;
         $email = $faker->email;
-        $password = Hash::make('password');
+        $password = Str::password(10, true, true, false, false);
 
         $this->info('Generating random user...');
         $this->info('Name: ' . $name);
         $this->info('Email: ' . $email);
-        $this->info('Password: password');
+        $this->info('Password: '. $password);
 
         \App\Models\User::create([
             'name' => $name,
